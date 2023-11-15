3fc37033117d956b8526b1669ebd203947622ed4
liliblim <missololo@gmail.com>
2023-11-15 13:47:37 +0300

    Change

--- work3.1/lilihomework3.1/lilihomework3.1/lilihomework3.1.cpp
+++ work3.1/lilihomework3.1/lilihomework3.1/lilihomework3.1.cpp
@@ -1,7 +1,4 @@
-﻿// lilihomework3.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 class Calculator {
 private:
@@ -78,7 +75,7 @@ int main() {
         std::cout << "Введите num1: ";
         std::cin >> input_num1;
 
-        if (input_num1 == 0) {
+        if (!calculator.set_num1(input_num1)) {
             std::cout << "Неверный ввод!" << std::endl;
             continue;
         }
@@ -86,20 +83,19 @@ int main() {
         std::cout << "Введите num2: ";
         std::cin >> input_num2;
 
-        if (input_num2 == 0) {
+        if (!calculator.set_num2(input_num2)) {
             std::cout << "Неверный ввод!" << std::endl;
             continue;
         }
 
-        if (calculator.set_num1(input_num1) && calculator.set_num2(input_num2)) {
-            std::cout << "num1 + num2 = " << calculator.add() << std::endl;
-            std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
-            std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
-            std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
-            std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
-            std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
-            user_num++;
-        }
+        std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+        std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
+        user_num++;
+
         if (user_num >= 2) {
             break;
         }
@@ -107,15 +103,3 @@ int main() {
 
     return 0;
 }
-
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
