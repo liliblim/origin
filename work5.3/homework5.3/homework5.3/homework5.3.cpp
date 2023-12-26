4ca1b660b80d5ad4c9f0f1ae3f580d259f03aae7
liliblim <missololo@gmail.com>
2023-12-26 07:52:29 +0300

    Change work5.3

--- work5.3/homework5.3/homework5.3/homework5.3.cpp
+++ work5.3/homework5.3/homework5.3/homework5.3.cpp
@@ -1,137 +1,104 @@
 ﻿#include <iostream>
 #include <cmath>
-#include <Windows.h>
-
+#include <vector>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 class Figure {
 public:
     virtual void print_info() const {
-        std::cout << "Фигура:" << std::endl;
-        std::cout << (check() ? "Правильная" : "Неправильная") << std::endl;
-        std::cout << "Количество сторон: " << sides << std::endl;
+        std::cout << "Фигура:\n"
+            << (check() ? "Правильная" : "Неправильная") << "\n"
+            << "Количество сторон: " << sides.size() << "\n";
     }
 
     virtual bool check() const {
-        // Проверка общих условий для фигуры (количество сторон)
-        return sides == 0;
+        return sides.size() > 0; // Проверка наличия сторон
     }
 
 protected:
-    int sides = 0;  // Общее количество сторон у всех фигур
+    std::vector<double> sides;
 };
 
 class Triangle : public Figure {
 public:
-    Triangle(double side1, double side2, double side3)
-        : a(side1), b(side2), c(side3) {
-        sides = 3;
-        A = acos((b * b + c * c - a * a) / (2 * b * c)) * 180.0 / 3.14159265358979323846;
-        B = acos((c * c + a * a - b * b) / (2 * c * a)) * 180.0 / 3.14159265358979323846;
-        C = 180 - A - B;
-    }
-
-    Triangle(double side1, double side2, bool isRightTriangle)
-        : a(side1), b(side2) {
-        sides = 3;
-        A = atan2(side2, side1) * 180.0 / 3.14159265358979323846;
-        B = 90;
-        C = 180 - A - B;
-    }
-
-    Triangle(double side)
-        : a(side), b(side), c(side) {
-        sides = 3;
-        A = 60;
-        B = 60;
-        C = 60;
+    Triangle(double side_a, double side_b, double side_c) {
+        sides = { side_a, side_b, side_c };
+        calculateAngles();
     }
 
     void print_info() const override {
-        std::cout << "Треугольник:" << std::endl;
-        std::cout << (check() ? "Правильный" : "Неправильный") << std::endl;
-        std::cout << "Количество сторон: " << sides << std::endl;
-        std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
-        std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl;
+        std::cout << "Треугольник:\n";
+        Figure::print_info();
+        if (check()) {
+            std::cout << "Стороны: a=" << sides[0] << " b=" << sides[1] << " c=" << sides[2] << "\n"
+                << "Углы: A=" << angles[0] << " B=" << angles[1] << " C=" << angles[2] << "\n";
+        }
     }
 
     bool check() const override {
-        // Проверка суммы углов в треугольнике
-        return Figure::check() && A + B + C == 180;
+        if (Figure::check() && sides.size() == 3) {
+            // Проверка наличия трех сторон
+            return angles[0] + angles[1] + angles[2] == 180.0; // Проверка суммы углов в треугольнике
+        }
+        return false;
     }
 
 private:
-    double a, b, c;  // Длины сторон
-    double A, B, C;  // Углы
+    std::vector<double> angles;
+
+    void calculateAngles() {
+        angles.resize(3);
+        for (size_t i = 0; i < 3; ++i) {
+            angles[i] = acos((sides[(i + 1) % 3] * sides[(i + 1) % 3] +
+                sides[(i + 2) % 3] * sides[(i + 2) % 3] -
+                sides[i] * sides[i]) / (2 * sides[(i + 1) % 3] * sides[(i + 2) % 3])) * 180.0 / M_PI;
+        }
+    }
 };
 
-class Quadrangle : public Figure {
+class Rectangle : public Figure {
 public:
-    Quadrangle(double side1, double side2, double side3, double side4)
-        : a(side1), b(side2), c(side3), d(side4) {
-        sides = 4;
-        A = 90;
-        B = 90;
-        C = 90;
-        D = 90;
-    }
-
-    Quadrangle(double base1, double height1, double base2, double height2, bool isRectangle = false)
-        : a(base1), b(height1), c(base2), d(height2) {
-        if (isRectangle) {
-            A = 90;
-            B = 90;
-            C = 90;
-            D = 90;
-        }
-        else {
-            A = acos((b * b + c * c - a * a) / (2 * b * c)) * 180.0 / 3.14159265358979323846;
-            B = acos((c * c + d * d - b * b) / (2 * c * d)) * 180.0 / 3.14159265358979323846;
-            C = acos((d * d + a * a - c * c) / (2 * d * a)) * 180.0 / 3.14159265358979323846;
-            D = 180 - A - B - C;
-        }
+    Rectangle(double side_a, double side_b) {
+        sides = { side_a, side_b, side_a, side_b };
     }
 
     void print_info() const override {
-        std::cout << "Четырёхугольник:" << std::endl;
-        std::cout << (check() ? "Правильный" : "Неправильный") << std::endl;
-        std::cout << "Количество сторон: " << sides << std::endl;
-        std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << " d=" << d << std::endl;
-        std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << " D=" << D << std::endl;
+        std::cout << "Прямоугольник:\n";
+        Figure::print_info();
+        if (check()) {
+            std::cout << "Стороны: a=" << sides[0] << " b=" << sides[1] << " c=" << sides[2] << " d=" << sides[3] << "\n";
+        }
     }
 
     bool check() const override {
-        // Проверка суммы углов в четырёхугольнике
-        return Figure::check() && A + B + C + D == 360;
+        if (Figure::check() && sides.size() == 4) {
+            // Проверка наличия четырех сторон
+            return sides[0] == sides[2] && sides[1] == sides[3]; // Проверка равенства сторон
+        }
+        return false;
     }
-
-private:
-    double a, b, c, d;  // Длины сторон
-    double A, B, C, D;  // Углы
 };
+
+// Добавьте аналогичные классы для других фигур
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    SetConsoleOutputCP(1251);
-    SetConsoleCP(1251);
-    Triangle triangle(10, 20, 30);
-    Triangle rightTriangle(10, 20, true);
-    Triangle isoscelesTriangle(10, 20, 50);
-    Triangle equilateralTriangle(30);
-    Quadrangle quadrangle(10, 20, 30, 40);
+    std::vector<Figure*> figures;
 
-    triangle.print_info();
-    std::cout << std::endl;
+    figures.push_back(new Figure());
+    figures.push_back(new Triangle(10, 20, 30));
+    figures.push_back(new Rectangle(10, 20));
+    // Добавьте создание экземпляров других фигур
 
-    rightTriangle.print_info();
-    std::cout << std::endl;
+    for (const auto& figure : figures) {
+        figure->print_info();
+        std::cout << "\n";
+    }
 
-    isoscelesTriangle.print_info();
-    std::cout << std::endl;
-
-    equilateralTriangle.print_info();
-    std::cout << std::endl;
-
-    quadrangle.print_info();
+    // Освобождение памяти
+    for (const auto& figure : figures) {
+        delete figure;
+    }
 
     return 0;
 }
-
